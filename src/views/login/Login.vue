2043f9f850a103c7d17f7170d07cbc05ab0cb90c
张伟刚 <1944452035@qq.com>
2021-02-01 23:13:34 +0800

    youhua

---
+++ src/views/login/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="login">
+    <el-form class="login_form" ref="loginFormRef" :model="loginForm" :rules="loginFormRules">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="loginForm.username" placeholder="请输入用户名" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="loginForm.password" placeholder="请输入密码" show-password></el-input>
+      </el-form-item>
+      <el-form-item>
+        <div>
+          <el-button type="success" @click="login">登录</el-button>
+          <el-button type="danger" @click="reset">重置</el-button>
+        </div>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名称", trigger: "blur" },
+          { min: 2, max: 4, message: "长度在 2 到 4 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入用户密码", trigger: "blur" },
+          { min: 6, max: 15, message: "密码长度至少6个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    login() {
+      this.$refs.loginFormRef.validate(valid => {
+        if (valid) {
+          alert("submit!");
+          this.$router.push("/home");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    reset() {
+      this.$refs.loginFormRef.resetFields();
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -2,22 +2,80 @@
   <div>
     <!-- 背景轮播图 -->
     <swiper-back></swiper-back>
-    <index-info></index-info>
+    <div class="main">
+      <div v-if="bool">
+        <login-info ref></login-info>
+      </div>
+      <div v-else>
+        <register-info ref></register-info>
+      </div>
+      <div class="reg_btn">
+        <button ref="doClick" class="change_btn" @click="getUrl">——点击注册——</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import SwiperBack from "@/components/common/swiper.vue";
-import indexInfo from "../index.vue";
+import LoginInfo from "@/views/login/Login.vue";
+import RegisterInfo from "@/views/register/Register.vue";
 export default {
   name: "Mian",
   data() {
-    return {};
+    return {
+      bool: true
+    };
   },
-  components: { SwiperBack, indexInfo },
-  methods: {}
+  components: { SwiperBack, LoginInfo, RegisterInfo },
+  methods: {
+    // 路径切换
+    getUrl() {
+      this.bool = this.bool ? false : true;
+      return this.bool;
+    }
+  }
 };
 </script>
 
 <style lang="less" scoped>
+html,
+body {
+  position: relative;
+}
+.main {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: 99;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: auto;
+  padding: 20px 30px;
+  border-radius: 10px;
+  background-color: rgb(138, 231, 138);
+  box-shadow: 0 0px 10px #868383;
+
+  .reg_btn {
+    text-align: right;
+    font-size: 15px;
+    .change_btn {
+      padding: 5px 10px;
+      background-color: #8ae78a;
+    }
+  }
+
+  .login {
+    height: 400px;
+    .login_form {
+      padding: 70px 0 0 100px;
+    }
+  }
+  .el-input {
+    width: 70%;
+  }
+  .el-form-item__error {
+    font-size: 18px !important;
+  }
+}
 </style>
